7bacef7fc5e12c2d85388875c742049e68a7c0b9
Aishani Singh <66542645+aishanisingh@users.noreply.github.com>
2021-05-24 17:30:42 -0700

    Update about-me.js

--- pages/posts/about-me.js
+++ pages/posts/about-me.js
@@ -1,3 +1,13 @@
 export default function aboutme() {
-    return <h1>Hi!</h1>
+    export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>About Me</title>
+      </Head>
+      <h1>About Me</h1>
+      <p>Hello, my name is Aishani! I like to code a lot :) </p>
+    </Layout>
+  )
+}
   }
